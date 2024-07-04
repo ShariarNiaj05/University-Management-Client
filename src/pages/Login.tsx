9ef7e006a72b616665394f38df4c5b5b526a02f6
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-05 00:10:07 +0600

    simple for and mutation connection

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,28 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
-  return <div>login</div>;
+  const [login, { data, error }] = useLoginMutation();
+
+  console.log({ data }, { error });
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">id: </label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">password: </label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
 };
 
 export default Login;

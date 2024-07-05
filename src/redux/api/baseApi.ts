17e362b1cdad7dad2459681ceceecf0e19174a47
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-06 01:21:11 +0600

    Retrieve New Access Token Using Refresh Token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,12 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://university-management-server-alpha.vercel.app/api/v1",
@@ -16,8 +23,34 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+const baseQueryWithRefreshToken: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
   const result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 401) {
+    // sending refresh token to generate new access token
+    const res = await fetch(
+      "https://university-management-server-alpha.vercel.app/auth/refresh-token",
+      // "http://localhost:5000/auth/refresh-token",
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
+    const data = await res.json();
+    console.log("refresh data", data);
+    const user = (api.getState() as RootState).auth.user;
+
+    api.dispatch(
+      setUser({
+        user,
+        token: data?.data?.accessToken,
+      })
+    );
+  }
   return result;
 };
 

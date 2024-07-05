b04447a3035278f48fc7224a0458248aab7d95df
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-05 20:27:56 +0600

    Login Redirect And Logout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,8 +2,10 @@ import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 type TData = {
   id: string;
@@ -12,6 +14,8 @@ type TData = {
 const Login = () => {
   const [login, { error }] = useLoginMutation();
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
   const { register, handleSubmit } = useForm({
     defaultValues: {
       id: "A-0001",
@@ -19,23 +23,31 @@ const Login = () => {
     },
   });
   const onSubmit = async (data) => {
-    const userInfo = {
-      id: data.id,
-      password: data.password,
-    };
+    const toastId = toast.loading("Logging in");
+    try {
+      const userInfo = {
+        id: data.id,
+        password: data.password,
+      };
 
-    const res = await login(userInfo).unwrap();
+      const res = await login(userInfo).unwrap();
 
-    const user = verifyToken(res?.data?.accessToken);
+      const user = verifyToken(res?.data?.accessToken) as TUser;
 
-    dispatch(
-      setUser({
-        user: user,
-        token: res?.data?.accessToken,
-      })
-    );
-
-    console.log(user);
+      dispatch(
+        setUser({
+          user: user,
+          token: res?.data?.accessToken,
+        })
+      );
+      toast.success("Log in successful", { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
+    } catch (error) {
+      toast.error("Something went wrong during login", {
+        id: toastId,
+        duration: 2000,
+      });
+    }
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>

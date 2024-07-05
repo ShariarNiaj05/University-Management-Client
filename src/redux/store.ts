a289b02f7108bca6780099e4894d9d351f304fcc
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-05 15:48:11 +0600

    redux persistant to hold the auth state

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import { baseApi } from "./api/baseApi";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {

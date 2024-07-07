9aa583650795ccfee772ad42b8df106241def586
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-08 00:13:21 +0600

    type issue resolve

--- src/components/form/WrapperForm.tsx
+++ src/components/form/WrapperForm.tsx
@@ -6,21 +6,20 @@ import {
   useForm,
 } from "react-hook-form";
 
+type TFormConfig = {
+  defaultValues?: Record<string, any>;
+};
 type TFormProps = {
   onSubmit: SubmitHandler<FieldValues>;
   children: ReactNode;
-  defaultValues: {
-    id: string;
-    password: string;
-  };
-};
+} & TFormConfig;
 
 const WrapperForm = ({ onSubmit, children, defaultValues }: TFormProps) => {
-  const formConfig = {};
+  const formConfig: TFormConfig = {};
   if (defaultValues) {
     formConfig["defaultValues"] = defaultValues;
   }
-  const methods = useForm({ formConfig });
+  const methods = useForm(formConfig);
 
   return (
     <FormProvider {...methods}>

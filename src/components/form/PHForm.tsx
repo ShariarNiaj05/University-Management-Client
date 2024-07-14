b5028afdd2cb1e2e76b0dec8900c56e270bf7042
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-15 00:02:09 +0600

    previous form field refactor

--- src/components/form/PHForm.tsx
+++ src/components/form/PHForm.tsx
@@ -1,10 +1,11 @@
-import { ReactNode } from 'react';
+import { Form } from "antd";
+import { ReactNode } from "react";
 import {
   FieldValues,
   FormProvider,
   SubmitHandler,
   useForm,
-} from 'react-hook-form';
+} from "react-hook-form";
 
 type TFormConfig = {
   defaultValues?: Record<string, any>;
@@ -19,14 +20,16 @@ const PHForm = ({ onSubmit, children, defaultValues }: TFormProps) => {
   const formConfig: TFormConfig = {};
 
   if (defaultValues) {
-    formConfig['defaultValues'] = defaultValues;
+    formConfig["defaultValues"] = defaultValues;
   }
 
   const methods = useForm(formConfig);
 
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+      <Form layout="vertical" onFinish={methods.handleSubmit(onSubmit)}>
+        {children}
+      </Form>
     </FormProvider>
   );
 };

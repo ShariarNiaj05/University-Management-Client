085978618d0621c387724e0d4be6591392039caa
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-15 01:10:45 +0600

    fixing layout error

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,8 +1,8 @@
-import { Button, Layout } from 'antd';
-import Sidebar from './Sidebar';
-import { useAppDispatch } from '../../redux/hooks';
-import { logout } from '../../redux/features/auth/authSlice';
-import { Outlet } from 'react-router-dom';
+import { Button, Layout } from "antd";
+import Sidebar from "./Sidebar";
+import { useAppDispatch } from "../../redux/hooks";
+import { logout } from "../../redux/features/auth/authSlice";
+import { Outlet } from "react-router-dom";
 const { Header, Content } = Layout;
 
 const MainLayout = () => {
@@ -13,13 +13,13 @@ const MainLayout = () => {
   };
 
   return (
-    <Layout style={{ height: '100vh' }}>
+    <Layout style={{ height: "100%" }}>
       <Sidebar />
       <Layout>
         <Header>
-          <Button onClick={handleLogout}>Logout</Button>{' '}
+          <Button onClick={handleLogout}>Logout</Button>{" "}
         </Header>
-        <Content style={{ margin: '24px 16px 0' }}>
+        <Content style={{ margin: "24px 16px 0" }}>
           <div
             style={{
               padding: 24,

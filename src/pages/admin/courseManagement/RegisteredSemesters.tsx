78024c8cb3b3ced4c0dcd588a9b26f42b5f59b23
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-19 00:35:03 +0600

    table column added

--- src/pages/admin/courseManagement/RegisteredSemesters.tsx
+++ src/pages/admin/courseManagement/RegisteredSemesters.tsx
@@ -34,49 +34,21 @@ const RegisteredSemesters = () => {
       title: "Name",
       key: "name",
       dataIndex: "name",
-      filters: [
-        {
-          text: "Autumn",
-          value: "Autumn",
-        },
-        {
-          text: "Fall",
-          value: "Fall",
-        },
-        {
-          text: "Summer",
-          value: "Summer",
-        },
-      ],
     },
     {
-      title: "Year",
-      key: "year",
-      dataIndex: "year",
-      filters: [
-        {
-          text: "2024",
-          value: "2024",
-        },
-        {
-          text: "2025",
-          value: "2025",
-        },
-        {
-          text: "2026",
-          value: "2026",
-        },
-      ],
+      title: "Status",
+      key: "status",
+      dataIndex: "status",
     },
     {
-      title: "Start Month",
-      key: "startMonth",
-      dataIndex: "startMonth",
+      title: "Start Date",
+      key: "startDate",
+      dataIndex: "startDate",
     },
     {
-      title: "End Month",
-      key: "endMonth",
-      dataIndex: "endMonth",
+      title: "End Date",
+      key: "endDate",
+      dataIndex: "endDate",
     },
     {
       title: "Action",

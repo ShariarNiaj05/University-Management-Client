02578876c24150ace49ae5b0715c8618cc78e086
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-22 00:22:14 +0600

    create course started

--- src/pages/admin/courseManagement/CreateCourse.tsx
+++ src/pages/admin/courseManagement/CreateCourse.tsx
@@ -1,5 +1,65 @@
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
+import { semesterStatusOptions } from "../../../constants/semester";
+import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
+import { toast } from "sonner";
+import PHDatePicker from "../../../components/form/PHDatePicker";
+import PHInput from "../../../components/form/PHInput";
+import { useAddRegisteredSemesterMutation } from "../../../redux/features/admin/courseManagement.api";
+import { TResponse } from "../../../types";
+
 const CreateCourse = () => {
-  return <div>CreateCourse</div>;
+  const [addSemester] = useAddRegisteredSemesterMutation();
+  const { data: academicSemester } = useGetAllSemestersQuery([
+    { name: "sort", value: "year" },
+  ]);
+  const academicSemesterOptions = academicSemester?.data?.map((item) => ({
+    value: item._id,
+    label: `${item.name} ${item.year}`,
+  }));
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Creating...");
+
+    const semesterData = {
+      ...data,
+      minCredit: Number(data?.minCredit),
+      maxCredit: Number(data?.maxCredit),
+    };
+
+    try {
+      const res = (await addSemester(semesterData)) as TResponse<any>;
+      console.log(res);
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("Semester created", { id: toastId });
+      }
+    } catch (err) {
+      toast.error("Something went wrong", { id: toastId });
+    }
+    console.log(semesterData);
+  };
+
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm
+          onSubmit={onSubmit}
+          // resolver={zodResolver(academicSemesterSchema)}
+        >
+          <PHInput type="text" name="title" label="Min Credit" />
+          <PHInput type="text" name="prefix" label="Max Credit" />
+          <PHInput type="text" name="maxCredit" label="Max Credit" />
+          <PHInput type="text" name="maxCredit" label="Max Credit" />
+          <PHInput type="text" name="maxCredit" label="Max Credit" />
+
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default CreateCourse;

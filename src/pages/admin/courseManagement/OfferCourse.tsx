7eeedc4895201e3f5fed2469225305a38c0315b7
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-22 23:38:43 +0600

    select form with watch

--- src/pages/admin/courseManagement/OfferCourse.tsx
+++ src/pages/admin/courseManagement/OfferCourse.tsx
@@ -3,8 +3,13 @@ import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
 import { useGetAcademicFacultiesQuery } from "../../../redux/features/admin/academicManagement.api";
+import { useState } from "react";
 
 const OfferCourse = () => {
+  const [id, setId] = useState("");
+
+  console.log("Inside parent component", id);
+
   const { data: academicFacultyData } = useGetAcademicFacultiesQuery(undefined);
 
   const academicSemesterOptions = academicFacultyData?.data?.map((item) => ({
@@ -15,6 +20,7 @@ const OfferCourse = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
+
   return (
     <Flex justify="center" align="center">
       <Col span={6}>

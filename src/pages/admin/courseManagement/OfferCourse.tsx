7f138159ddca78e05d8d5b4d60c83fece06e4d08
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-22 23:23:13 +0600

    offered course form

--- src/pages/admin/courseManagement/OfferCourse.tsx
+++ src/pages/admin/courseManagement/OfferCourse.tsx
@@ -1,5 +1,23 @@
+import { Button, Col, Flex } from "antd";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+
 const OfferCourse = () => {
-  return <div>OfferCourse</div>;
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="test" label="Test" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default OfferCourse;

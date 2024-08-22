bdfa25d4aae4e6592beae6079ef47f7aa627d119
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-22 21:45:33 +0600

    added course table

--- src/pages/admin/courseManagement/Courses.tsx
+++ src/pages/admin/courseManagement/Courses.tsx
@@ -1,5 +1,57 @@
+import { Button, Table } from "antd";
+import { useGetAllCoursesQuery } from "../../../redux/features/admin/courseManagement.api";
+
 const Courses = () => {
-  return <div>Courses</div>;
+  // const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+
+  const { data: courses, isFetching } = useGetAllCoursesQuery(undefined);
+
+  const tableData = courses?.data?.map(({ _id, title, prefix, code }) => ({
+    key: _id,
+    title,
+    code: `${prefix}${code}`,
+  }));
+
+  const columns = [
+    {
+      title: "Title",
+      key: "title",
+      dataIndex: "title",
+    },
+    {
+      title: "Code",
+      key: "code",
+      dataIndex: "code",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: () => {
+        return <Button>Assign Faculty</Button>;
+      },
+    },
+  ];
+
+  // const onChange: TableProps<TTableData>['onChange'] = (
+  //   _pagination,
+  //   filters,
+  //   _sorter,
+  //   extra
+  // ) => {
+  //   if (extra.action === 'filter') {
+  //     const queryParams: TQueryParam[] = [];
+  //     setParams(queryParams);
+  //   }
+  // };
+
+  return (
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      // onChange={onChange}
+    />
+  );
 };
 
 export default Courses;

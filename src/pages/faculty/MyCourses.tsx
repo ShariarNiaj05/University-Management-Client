2566bd846864961fafa25e4886d63981a506121b
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-24 00:56:34 +0600

    Faculty Courses data management

--- src/pages/faculty/MyCourses.tsx
+++ src/pages/faculty/MyCourses.tsx
@@ -1,12 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import { useGetAllFacultyCoursesQuery } from "../../redux/features/faculty/facultyCourses.api";
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../components/form/PHForm";
+import PHSelect from "../../components/form/PHSelect";
 
 const MyCourses = () => {
   const { data: facultyCoursesData } = useGetAllFacultyCoursesQuery(undefined);
   const navigate = useNavigate();
   console.log(facultyCoursesData);
 
-  return <div>faculty offer course</div>;
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelect
+            options={semesterOptions}
+            name="semesterRegistration"
+            label="Semester"
+          />
+          <PHSelect options={courseOptions} name="course" label="Course" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default MyCourses;

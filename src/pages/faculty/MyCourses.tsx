b9c150e1d9d1b9b1b32aeb5f91111344938758e6
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-24 00:53:22 +0600

    faculty courses data query

--- src/pages/faculty/MyCourses.tsx
+++ src/pages/faculty/MyCourses.tsx
@@ -1,4 +1,11 @@
+import { useNavigate } from "react-router-dom";
+import { useGetAllFacultyCoursesQuery } from "../../redux/features/faculty/facultyCourses.api";
+
 const MyCourses = () => {
+  const { data: facultyCoursesData } = useGetAllFacultyCoursesQuery(undefined);
+  const navigate = useNavigate();
+  console.log(facultyCoursesData);
+
   return <div>faculty offer course</div>;
 };
 

d036ef799f959f952535debacd13e61c825aa53e
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-23 22:43:45 +0600

    login logic updated

--- src/pages/ChangePassword.tsx
+++ src/pages/ChangePassword.tsx
@@ -1,5 +1,31 @@
+import { Button, Row } from "antd";
+import PHForm from "../components/form/PHForm";
+import PHInput from "../components/form/PHInput";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { useChangePasswordMutation } from "../redux/features/admin/userManagement.api";
+import { TResponse } from "../types";
+import { useAppDispatch } from "../redux/hooks";
+import { logout } from "../redux/features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+
 const ChangePassword = () => {
-  return <div>change-password</div>;
+  const [changePassword] = useChangePasswordMutation();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Row justify="center" align="middle" style={{ height: "100vh" }}>
+      <PHForm onSubmit={onSubmit}>
+        <PHInput type="text" name="oldPassword" label="Old Password" />
+        <PHInput type="text" name="newPassword" label="New Password" />
+        <Button htmlType="submit">Login</Button>
+      </PHForm>
+    </Row>
+  );
 };
 
 export default ChangePassword;

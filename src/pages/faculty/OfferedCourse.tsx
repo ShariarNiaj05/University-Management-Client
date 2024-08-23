6b96bdbde309c5d4414005ec0e23868a58bbbfc6
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-23 23:53:33 +0600

    offered course data modification

--- src/pages/faculty/OfferedCourse.tsx
+++ src/pages/faculty/OfferedCourse.tsx
@@ -1,8 +1,19 @@
 import { useGetAllOfferedCoursesQuery } from "../../redux/features/student/studentCourseApi";
 
 const OfferedCourse = () => {
-  const { data } = useGetAllOfferedCoursesQuery(undefined);
-  console.log(data);
+  const { data: offeredCourseData } = useGetAllOfferedCoursesQuery(undefined);
+  const [enroll] = useEnrolCourseMutation();
+
+  const singleObject = offeredCourseData?.data?.reduce((acc, item) => {
+
+    acc["courseTitle"] = acc.
+    return acc;
+  }, {});
+
+  
+  const modifiedData = Object.values(singleObject ? singleObject : {});
+
+  
   return (
     <div>
       <h1> This is OfferedCourse component </h1>

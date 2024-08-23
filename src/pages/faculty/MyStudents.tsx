3730fc29c4197f5cf7b139f5620470c1d6659656
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-24 01:21:49 +0600

    my students table

--- src/pages/faculty/MyStudents.tsx
+++ src/pages/faculty/MyStudents.tsx
@@ -1,6 +1,12 @@
 import { useParams } from "react-router-dom";
-import { useGetAllFacultyCoursesQuery } from "../../redux/features/faculty/facultyCourses.api";
-import { Table } from "antd";
+import {
+  useAddMarkMutation,
+  useGetAllFacultyCoursesQuery,
+} from "../../redux/features/faculty/facultyCourses.api";
+import { Button, Modal, Table } from "antd";
+import { useState } from "react";
+import PHForm from "../../components/form/PHForm";
+import PHInput from "../../components/form/PHInput";
 
 const MyStudents = () => {
   const { registerSemesterId, courseId } = useParams();
@@ -39,6 +45,34 @@ const MyStudents = () => {
     },
   ];
   return <Table columns={columns} dataSource={tableData} />;
+};
+
+const AddMarksModal = ({ studentInfo }) => {
+  console.log(studentInfo);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [addMark] = useAddMarkMutation();
+
+  return (
+    <>
+      <Button onClick={showModal}>Add Faculty</Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        // onCancel={handleCancel}
+        footer={null}
+      >
+        <PHForm
+        //   onSubmit={handleSubmit}
+        >
+          <PHInput type="text" name="classTest1" label="Class Test 1" />
+          <PHInput type="text" name="classTest2" label="Class Test 2" />
+          <PHInput type="text" name="midTerm" label="Midterm" />
+          <PHInput type="text" name="finalTerm" label="Final" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Modal>
+    </>
+  );
 };
 
 export default MyStudents;

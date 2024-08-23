3d842fa9e7cf18839badd500f111f6758f2d15a9
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-24 01:16:08 +0600

    my students table

--- src/pages/faculty/MyStudents.tsx
+++ src/pages/faculty/MyStudents.tsx
@@ -1,5 +1,23 @@
+import { useParams } from "react-router-dom";
+import { useGetAllFacultyCoursesQuery } from "../../redux/features/faculty/facultyCourses.api";
+
 const MyStudents = () => {
   const { registerSemesterId, courseId } = useParams();
+  const { data: facultyCoursesData } = useGetAllFacultyCoursesQuery([
+    { name: "semesterRegistration", value: registerSemesterId },
+    { name: "course", value: courseId },
+  ]);
+
+  const tableData = facultyCoursesData?.data?.map(
+    ({ _id, student, semesterRegistration, offeredCourse }) => ({
+      key: _id,
+      name: student.fullName,
+      roll: student.id,
+      semesterRegistration: semesterRegistration._id,
+      student: student._id,
+      offeredCourse: offeredCourse._id,
+    })
+  );
 
   return <div>MyStudents</div>;
 };
